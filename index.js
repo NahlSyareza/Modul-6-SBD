05585fbc93c9b6a9bdc00d5e6a98da531d8024c7
NahlSyareza <availablearner@protonmail.com>
2024-04-28 20:33:49 +0700

    TUTAM Modul 6 Done

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const eventRepo = require("./repositories/repository.event");
+const logger = require("./tools/logger");
+const morganMiddleware = require("./middlewares/middleware.logging");
+const { default: helmet } = require("helmet");
 
 const port = 1340;
 const app = express();
 
 // Middleware
+app.use(morganMiddleware);
+app.use(helmet);
 app.use(bodyParser.json());
 
 // Endpoint
-app.post('/events', eventRepo.addEvent);
-app.get('/events', eventRepo.getAllEvents);
-app.put('/events/:id', eventRepo.updateEvent);
-app.delete('/events/:id', eventRepo.deleteEvent);
+app.post("/events", eventRepo.addEvent);
+app.get("/events", eventRepo.getAllEvents);
+app.put("/events/:id", eventRepo.updateEvent);
+app.delete("/events/:id", eventRepo.deleteEvent);
 
-app.post('/events/bulk', eventRepo.bulk);
-app.get('/events/country', eventRepo.country);
-app.get('/events/paginate', eventRepo.paginate);
+app.post("/events/bulk", eventRepo.bulk);
+app.get("/events/country", eventRepo.country);
+app.get("/events/paginate", eventRepo.paginate);
 
 app.listen(port, () => {
-	console.log("Zoup Voum. Server is running and listening on port!", port);
+  logger.info("Server is running!!!");
 });
